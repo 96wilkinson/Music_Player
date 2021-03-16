e5f5c4268eed0251bca961e1c9871a0e9aaa539e
unknown <96wilkinson@sky.com>
2021-03-16 19:26:02 +0000

    clicking on a song displays the correct title

--- src/front-end/components/getSongsTable.js
+++ src/front-end/components/getSongsTable.js
@@ -4,17 +4,25 @@ import axios from 'axios'
 export default class getSongsTable extends React.Component {
     constructor() {
         super();
-        this.state = { items: [] }
+        this.state = {
+            items: [],
+            selectedTrack: "nothing as of yet"
+        }
     }
     componentWillMount() {
         axios.post('http://localhost:3001/getData')
             .then(response => this.setState({ items: response.data }))
     }
 
+    onViewChange = (id) => {
+        this.setState({selectedTrack: id}, () => console.log(this.state.id)) 
+      };
 
     render() {
         let items = this.state.items;
+        let track = this.state.selectedTrack;
         console.log(items)
+        
         return (
             <div>
                 <h1 id='Table Title'>Songs</h1>
@@ -24,19 +32,30 @@ export default class getSongsTable extends React.Component {
                             <th>Title</th>
                             <th>Album</th>
                             <th>Length of the song</th>
+                            <th></th>
                         </tr>
-                        {items.map(item =>
-
-
-                            <tr key={item.id}>
-                                <td>{item.Title}</td>
-                                <td>{item.Album}</td>
-                                <td>{item.Time}</td>
+                        {items.map(items =>
+                            <tr key={items.id}>
+                                <td
+                                  id={items.Title}
+                                  onClick={() => this.onViewChange(items.Title)}>
+                                    Play
+                                </td>
+                                <td>
+                                    {items.Title}
+                                </td>
+                                <td>
+                                    {items.Album}
+                                </td>
+                                <td>
+                                    {items.Time}
+                                </td>
                             </tr>
 
                         )}
                     </tbody>
                 </table>
+                <h3>Playing: {track}</h3>
             </div>
         );
     }
