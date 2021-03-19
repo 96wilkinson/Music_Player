eb778d2d9098e1742390d41cb2f8df8bc8050e63
unknown <96wilkinson@sky.com>
2021-03-19 17:22:35 +0000

    added creation of db to data folder

--- src/Data/albumTable.sql
+++ src/Data/albumTable.sql
@@ -1,10 +1,11 @@
-CREATE TABLE 'album' (
-  'ID' int unsigned NOT NULL,
-  'Title' varchar(255) NOT NULL,
-  'Artist' varchar(255) NOT NULL,
-  'Time' int NOT NULL,
-  'Number' int NOT NULL,
-  'Album' varchar(255) NOT NULL,
-  PRIMARY KEY ('ID'),
-  UNIQUE KEY 'ID_UNIQUE' ('ID')
-)
+CREATE TABLE `album` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `Title` varchar(255) DEFAULT NULL,
+  `Artist` varchar(255) NOT NULL,
+  `Time` int NOT NULL,
+  `Number` int NOT NULL,
+  `Album` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
